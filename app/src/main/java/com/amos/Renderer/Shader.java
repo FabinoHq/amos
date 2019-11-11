b2263a01685d95b2da22575ca25fd4ec15a4f54b
FabinoHq <fabino.hq@gmail.com>
2019-11-11 14:16:43 +0100

    Add default shader sources

--- app/src/main/java/com/amos/Renderer/Shader.java
+++ app/src/main/java/com/amos/Renderer/Shader.java
@@ -50,35 +50,99 @@ import android.opengl.GLES20;
 public class Shader
 {
     ////////////////////////////////////////////////////////////////////////////
-    //  Shader default constructor                                            //
-    //  param vertexSrc : Vertex shader program source                        //
-    //  param fragmentSrc : Fragment shader program source                    //
+    //  Default vertex shader                                                 //
     ////////////////////////////////////////////////////////////////////////////
-    public Shader(String vertexSrc, String fragmentSrc)
+    static final String defaultVertexShaderSrc =
+        "attribute vec3 vertexPos;\n" +
+        "attribute vec2 vertexColor;\n" +
+        "uniform mat4 projMatrix;\n" +
+        "uniform mat4 viewMatrix;\n" +
+        "uniform mat4 modelMatrix;\n" +
+        "uniform float alphaUniform;\n" +
+        "varying vec2 texCoord;\n" +
+        "varying float alphaValue;\n" +
+        "void main()\n" +
+        "{\n" +
+        "   texCoord = vertexColor;\n" +
+        "   alphaValue = alphaUniform\n;" +
+        "   gl_Position = projMatrix*viewMatrix*modelMatrix*vec4(" +
+                "vertexPos, 1.0);\n" +
+        "}\n";
+
+    ////////////////////////////////////////////////////////////////////////////
+    //  Default fragment shader                                               //
+    ////////////////////////////////////////////////////////////////////////////
+    static final String defaultFragmentShaderSrc =
+        "precision mediump float;\n" +
+        "uniform sampler2D texture;\n" +
+        "varying vec2 texCoord;\n" +
+        "varying float alphaValue;\n" +
+        "void main()\n" +
+        "{\n" +
+        "   vec4 texColor = texture2D(texture, texCoord);\n" +
+        "   gl_FragColor = vec4(texColor.rgb, texColor.a*alphaValue);\n" +
+        "}\n";
+
+    ////////////////////////////////////////////////////////////////////////////
+    //  Shader default constructor                                            //
+    ////////////////////////////////////////////////////////////////////////////
+    public Shader()
     {
+        m_loaded = false;
+        m_vertexShaderSrc = "";
+        m_fragmentShaderSrc = "";
         m_vertexShader = 0;
         m_fragmentShader = 0;
         m_shaderProgram = 0;
+        m_textureLocation = -1;
+        m_projMatrixLocation = -1;
+        m_viewMatrixLocation = -1;
+        m_modelMatrixLocation = -1;
+        m_alphaLocation = -1;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //  init : Init shader                                                    //
+    //  param vertexSrc : Vertex shader source                                //
+    //  param fragmentSrc : Fragment shader source                            //
+    //  return : True if the shader is successfully loaded, false otherwise   //
+    ////////////////////////////////////////////////////////////////////////////
+    public boolean init(String vertexSrc, String fragmentSrc)
+    {
+        // Reset shader
+        m_loaded = false;
+        m_vertexShaderSrc = "";
+        m_fragmentShaderSrc = "";
+        m_vertexShader = 0;
+        m_fragmentShader = 0;
+        m_shaderProgram = 0;
+        m_textureLocation = -1;
+        m_projMatrixLocation = -1;
+        m_viewMatrixLocation = -1;
+        m_modelMatrixLocation = -1;
+        m_alphaLocation = -1;
+
+        // Set shader sources
+        m_vertexShaderSrc = vertexSrc;
+        m_fragmentShaderSrc = fragmentSrc;
 
         // Init vertex shader
         m_vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
 
         // Set vertex shader source
-        GLES20.glShaderSource(m_vertexShader, vertexSrc);
+        GLES20.glShaderSource(m_vertexShader, m_vertexShaderSrc);
 
         // Compile vertex shader
         GLES20.glCompileShader(m_vertexShader);
-
 
         // Init fragment shader
         m_fragmentShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
 
         // Set fragment shader source
-        GLES20.glShaderSource(m_fragmentShader, fragmentSrc);
+        GLES20.glShaderSource(m_fragmentShader, m_fragmentShaderSrc);
 
         // Compile fragment shader
         GLES20.glCompileShader(m_fragmentShader);
-
 
         // Create shader program
         m_shaderProgram = GLES20.glCreateProgram();
@@ -87,15 +151,69 @@ public class Shader
 
         // Link shader
         GLES20.glLinkProgram(m_shaderProgram);
+
+        // Use shader program
+        GLES20.glUseProgram(m_shaderProgram);
+
+        // Get texture location
+        m_textureLocation = GLES20.glGetUniformLocation(
+            m_shaderProgram,
+            "texture"
+        );
+        if (m_textureLocation == -1) { return false; }
+
+        // Get projection matrix location
+        m_projMatrixLocation = GLES20.glGetUniformLocation(
+            m_shaderProgram,
+            "projMatrix"
+        );
+        if (m_projMatrixLocation == -1) { return false; }
+
+        // Get view matrix location
+        m_viewMatrixLocation = GLES20.glGetUniformLocation(
+            m_shaderProgram,
+            "viewMatrix"
+        );
+        if (m_viewMatrixLocation == -1) { return false; }
+
+        // Get model matrix location
+        m_modelMatrixLocation = GLES20.glGetUniformLocation(
+            m_shaderProgram,
+            "modelMatrix"
+        );
+        if (m_modelMatrixLocation == -1) { return false; }
+
+        // Get alpha value location
+        m_alphaLocation = GLES20.glGetUniformLocation(
+            m_shaderProgram,
+            "alphaUniform"
+        );
+        if (m_alphaLocation == -1) { return false; }
+
+        GLES20.glUseProgram(0);
+
+        // Shader successfully loaded
+        m_loaded = true;
+        return true;
     }
 
 
-    // Vertex shader program
+    // Shader loaded status
+    private boolean m_loaded;
+
+    // Shader sources
+    private String m_vertexShaderSrc;
+    private String m_fragmentShaderSrc;
+
+    // Shader programs
     private int m_vertexShader;
-
-    // Fragment shader program
     private int m_fragmentShader;
-
-    // Shader program
     private int m_shaderProgram;
+
+    // Shader uniforms locations
+    private int m_textureLocation;
+    private int m_projMatrixLocation;
+    private int m_viewMatrixLocation;
+    private int m_modelMatrixLocation;
+    private int m_alphaLocation;
 }
